deb3706f8f22125bb8c371ed8cd423417469c273
Manfredi Lo Menzo <106279259+Manfrolm@users.noreply.github.com>
2023-07-15 10:47:44 +0200

    Update team.js

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-let newArr= []
+let newArr= [
+    {
+        name: "Manfredi", 
+        surname: "Lo Menzo", 
+        age: 21, 
+        city: "Palermo", 
+        hobby: "Videogame", 
+        favoriteFood: "Pizza", 
+        favoriteVideoGame: "Hollow Knight", 
+        favoriteFilm: "The Truman Show", 
+        favoriteBook: "Io non ho paura - Nicolò Ammaniti", 
+        petName: "Aaron"
+    },
+]
