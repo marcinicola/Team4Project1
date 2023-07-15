7654a404cb6e2a077eece1bf50d343a170ac02b4
DavidePiccica <139143867+DavidePiccica@users.noreply.github.com>
2023-07-15 10:47:55 +0200

    Update team.js

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-let newArr= []
+let newArr= [
+    {
+        name:"davide",
+        surname:"piccica",
+        age:25,
+        city:"palermo",
+        hobby:"sport",
+        favoriteFood:"hamburger",
+        favoriteVideoGame:"LOL",
+        favoriteFilm:"The Godfather",
+        favoriteBook:"A song of ice and fire",
+        petName:"niente"
+    },
+]
