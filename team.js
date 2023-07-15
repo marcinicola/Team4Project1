f0232a2d92593f2051a78d6d1fd1c56c581096a7
Airtcloudia <klau26@hotmail.it>
2023-07-15 11:18:43 +0200

    Update team.js

--- team.js
+++ team.js
@@ -1,1 +1,15 @@
-let newArr= []
+let newArr = [
+    {
+
+        name: "Claudia",
+        surname: "Ponti",
+        age: "25",
+        city: "Naples",
+        hobby: "Playing the piano, painting, Lego",
+        favoriteFood: "Pizza",
+        favoriteVideoGame: "Assasin Creed",
+        favoriteFilm: "Interstellar, Inception",
+        favoriteBook: "Viva la vida! di Frida Kahlo",
+        petName: "Tommy",
+    },
+]
