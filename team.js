2b4d1486c07022b14f73630ec0cdc0999c96fadb
Nicola Marcì <138504124+marcinicola@users.noreply.github.com>
2023-07-15 11:29:22 +0200

    Update team.js

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-let newArr= []
+let newArr = [
+    {
+        name: "Nicola",
+        surname: "Marcì",
+        age: 26,
+        city: "Siracusa",
+        hobby: "Gaming",
+        favotiteFood: "Panino carne di Cavallo",
+        favoriteVideoGame: "Call Of Duty",
+        favoriteFilm: "Shutter Island,Inception",
+        favoriteBook: "Educazione Siberiana",
+        petName: "Erik",
+    },
+]
