38be67422ce403a93b30a20e850c6841d650e094
Manfredi Lo Menzo <106279259+Manfrolm@users.noreply.github.com>
2023-07-15 11:11:41 +0200

    Update team.js

--- team.js
+++ team.js
@@ -1,14 +1,38 @@
-let newArr= [
-    {
-        name: "Manfredi", 
-        surname: "Lo Menzo", 
-        age: 21, 
-        city: "Palermo", 
-        hobby: "Videogame", 
-        favoriteFood: "Pizza", 
-        favoriteVideoGame: "Hollow Knight", 
-        favoriteFilm: "The Truman Show", 
-        favoriteBook: "Io non ho paura - Nicolò Ammaniti", 
-        petName: "Aaron"
-    },
-]
+let newArr = [
+      {
+        name: "Luca",
+        surname: "Campo",
+        age: 25,
+        city: "Gela",
+        hobby: "Play guitar and run",
+        favoriteFood: "Pizza",
+        favoriteVideoGame: "FIFA",
+        favoriteFilm: "The Wolf of Wall Street",
+        favoriteBook: "La solitudine dei numeri primi",
+        petName: "Ciak"
+      },
+      { 
+        name:"davide",
+        surname:"piccica",
+        age:25,
+        city:"palermo",
+        hobby:"sport",
+        favoriteFood:"hamburger",
+        favoriteVideoGame:"LOL",
+        favoriteFilm:"The Godfather",
+        favoriteBook:"A song of ice and fire",
+        petName:""
+      },
+      { 
+        name:"Manfredi",
+        surname:"Lo Menzo",
+        age:21,
+        city:"palermo",
+        hobby:"Videogiochi",
+        favoriteFood:"Pizza",
+        favoriteVideoGame:"Hollow Knight",
+        favoriteFilm:"The Truman Show",
+        favoriteBook:"Io non ho paura",
+        petName:"Aaron"
+      },
+   ]
