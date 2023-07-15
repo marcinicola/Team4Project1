52bd6e877b7c5ef2e2cbacc746f5b742ac176def
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-07-15 10:47:45 +0200

    add

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-let newArr= []
+let newArr = [
+    {
+        name: "Luca",
+        surname: "Campo",
+        age: 25,
+        city: "Gela",
+        hobby: "Play guitar and run",
+        favoriteFood: "Pizza",
+        favoriteVideoGame: "FIFA",
+        favoriteFilm: "The Wolf of Wall Street",
+        favoriteBook: "La solitudine dei numeri primi",
+        petName: "Ciak"
+    },
+]
